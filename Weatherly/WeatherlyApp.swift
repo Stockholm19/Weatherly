2dbec9facc91a4b249b2f73c531fc13480b24db2
Abrikozs <Abrikozs@gmail.com>
2025-04-07 01:31:59 +0300

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Роман Пшеничников on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
